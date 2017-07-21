06e063686527107a4a7d8df525b345bfaa9dad4b
郭广川 <ggc0402@qq.com>
2017-07-21 09:40:54 +0800

    add post

--- handler/purchase/signup_handler.py
+++ handler/purchase/signup_handler.py
@@ -17,7 +17,7 @@ class SignupHandler(BaseHandler):
         name = self.get_argument('name')
         phone = self.get_argument('phone')
         address = self.get_argument('address')
-        user_model = self.model_config.first(UserModel, union_id=self.session.get('union_id')) #type:UserModel
+        user_model = self.model_config.first(UserModel, union_id=self.session.get('union_id'))  # type:UserModel
         user_model.name = name
         user_model.phone = phone
         user_model.address = address
@@ -26,5 +26,22 @@ class SignupHandler(BaseHandler):
         self.render('purchase/pay.html', name=name, phone=phone, address=address, price=1)
         res = {
             'render': True
+        }
+        return res
+
+    @handler
+    def post(self):
+        name = self.get_argument('name')
+        phone = self.get_argument('phone')
+        address = self.get_argument('address')
+        user_model = self.model_config.first(UserModel, union_id=self.session.get('union_id'))  # type:UserModel
+        user_model.name = name
+        user_model.phone = phone
+        user_model.address = address
+        res = {
+            'name': name,
+            'phone': phone,
+            'address': address,
+            'price': 1
         }
         return res
