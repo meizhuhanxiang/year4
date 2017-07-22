0bc71d512305e74a5eeddee8b29baa5d42f7acd4
郭广川 <ggc0402@qq.com>
2017-07-22 18:07:44 +0800

    fix bug

--- handler/purchase/cheer_handler.py
+++ handler/purchase/cheer_handler.py
@@ -7,6 +7,8 @@ from handler.base.base_handler import BaseHandler, handler
 from handler.base.base_handler import BaseHandler
 from model.cheer import CheerModel
 from utils.exception import ServerError
+from model.order import OrderModel
+from model.user import UserModel
 
 __author__ = 'guoguangchuan'
 __email__ = 'ggc0402@qq.com'
@@ -23,9 +25,14 @@ class CheerHandler(BaseHandler):
 
         satisfy_cheer_num = int(utils.config.get('global', 'satisfy_cheer_num'))
         cheer_models = self.model_config.all(CheerModel, target_union_id=target_union_id)
+        user_model = self.model_config.first(UserModel, union_id=target_union_id)
+        order_model = self.model_config.first(OrderModel, user_id=user_model.id)
         target_cheer_num = len(cheer_models)
-        if target_cheer_num > satisfy_cheer_num:
+        if target_cheer_num >= satisfy_cheer_num:
             raise ServerError(ServerError.CHEER_IS_DONE)
+        else:
+            if order_model:
+                raise ServerError(ServerError.NO_DISCOT_BUY)
 
         cheer_model = CheerModel(union_id=union_id, target_union_id=target_union_id)
         self.model_config.add(cheer_model)
